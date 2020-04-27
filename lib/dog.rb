c784f59f7396ef501143e1d6f9a0439b5c9d5c09
Steve Womble <swomble@aol.com>
2020-04-27 20:37:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,10 +8,10 @@ class Dog
   end 
   
   def breed=(dogs_breed)
-    @@this_dogs_breed = dogs_breed
+    @this_dogs_breed = dogs_breed
   end 
   
   def breed
-    @@this_dogs_breed
+    @this_dogs_breed
   end 
 end
